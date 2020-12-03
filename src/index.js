cce6d7b1f7eea2ce350d38fe6afe7975a8602edf
Gourav Khator <gouravkhator9@gmail.com>
2020-12-03 13:48:28 +0530

    Removed PWA functionality due to UI issues, but the performance is fast as before

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import OfflineMessage from './components/OfflineMessage';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
-//Render OfflineMessage component for showing the user when they are offline
-ReactDOM.render(
-  <React.StrictMode>
-    <OfflineMessage />
-  </React.StrictMode>,
-  document.getElementById('internet-lost-message')
-);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,19 +8,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-serviceWorkerRegistration.register({
-  onUpdate: registration => {
-    //when we change anything in the code or files then it will trigger onUpdate 
-    //and we would get new data from network instead of cache
-
-    //display notification
-    let newContentNotification = document.getElementById('new-content');
-    newContentNotification.classList.remove('hidden');
-    setTimeout(() => {
-      newContentNotification.classList.add('hidden');
-    }, 15000);
-
-    registration.waiting.postMessage({ type: 'SKIP_WAITING' });
-  }
-});
